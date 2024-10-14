1c66f3fe5cc8f9223219a9bd6beaad934dbbdaf3
Francois Carstens <14223715+FranCarstens@users.noreply.github.com>
2024-10-14 11:11:12 -0400

    GitHub pages example (#8)
    * moved example to docs for github pages
    
    * Moved example for Github pages deploy
    
    * test run

--- src/index.js
+++ src/index.js
@@ -30,6 +30,8 @@ export function breadcrumbPlugin (hook, vm) {
 
     const ul = generateAccessibleBreadcrumb(homeLink, getListItems(readableUrlParts, urlParts, title))
 
+    console.log(vm)
+
     next(ul + html)
   })
 }
